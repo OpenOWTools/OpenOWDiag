88b0ef23c57ce243fa459a77a2bb22ff392daf1f
Brad <brad@4pi.com.au>
2022-05-07 16:43:02 +1000

    Initial blank commit

---
+++ src/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace OpenOWDiag;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
